2fdf341b1fa1156478731515ef74d01facdfd41c
salexsh <ShutovAlexandr@yandex.ru>
2023-02-16 22:38:25 +0300

    Updated dependencies. Still not working...

--- src/test/java/ru/study/odin/odinbot/OdinBotApplicationTests.java
+++ src/test/java/ru/study/odin/odinbot/OdinBotApplicationTests.java
@@ -1,9 +1,7 @@
 package ru.study.odin.odinbot;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OdinBotApplicationTests {
 
 	@Test
